c2a3f3703555c13ec1906b3f207de19357ba103d
phillip-vo <philvo1991@gmail.com>
2022-11-07 08:11:01 -0600

    style search components

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,32 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { Button, Container, Row, Carousel, Col, Image } from "react-bootstrap";
-import { checkTypes } from "../utils/utils";
+import React, { useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import PokemonCard from "./PokemonCard";
 import TypeCarousel from "./TypeCarousel";
+import { IoMdRefresh } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
 
 interface PokemonListProps {
   allPokemons: any[];
 }
-
-const types = [
-  "bug",
-  "dragon",
-  "electric",
-  "fairy",
-  "fighting",
-  "fire",
-  "flying",
-  "ghost",
-  "grass",
-  "ground",
-  "ice",
-  "normal",
-  "poison",
-  "psychic",
-  "rock",
-  "steel",
-  "water",
-];
 
 function PokemonList({ allPokemons }: PokemonListProps) {
   const pokemons = allPokemons || [];
@@ -36,26 +17,39 @@ function PokemonList({ allPokemons }: PokemonListProps) {
 
   return (
     <Container fluid className="dark-bg pb-5" style={{ paddingTop: "5rem" }}>
-      <Row className="border border-white m-5">
-        <Col lg={4} className="d-flex flex-column gap-3 border border-white">
-          <span className="" style={{ fontWeight: 700, fontSize: "1.5rem" }}>
-            Search by type:
-          </span>
-          <TypeCarousel setPokemonType={setPokemonType} />
-          {/* <div className="d-flex gap-2 flex-wrap border border-white">
-            {types.map((type) => (
-              <Image
-                src={checkTypes(type)}
-                width={50}
-                onClick={() => setPokemonType(type)}
-                className="type-icons"
+      <Row className="d-flex  justify-content-around m-5">
+        <Col lg={4}>
+          <div className="d-flex flex-column gap-3">
+            <span style={{ fontWeight: 700, fontSize: "1.5rem" }}>
+              Search by type:
+            </span>
+            <TypeCarousel setPokemonType={setPokemonType} />
+            <div className="d-flex justify-content-start m-3">
+              <Button
+                variant="outline-light"
+                className="rounded-circle"
+                onClick={() => setPokemonType("")}
+              >
+                <IoMdRefresh />
+              </Button>
+            </div>
+          </div>
+        </Col>
+        <Col lg={4}>
+          <div className="d-flex  flex-column gap-3">
+            <span style={{ fontWeight: 700, fontSize: "1.5rem" }}>
+              Search by name:
+            </span>
+            <div className="name-container">
+              <input
+                type="text"
+                placeholder="Name"
+                value={pokemonName}
+                onChange={(e) => setPokemonName(e.target.value)}
+                className="border border-light border-2 rounded bg-transparent w-100 name-input"
               />
-            ))}
-          </div> */}
-          <div className="d-flex m-2">
-            <Button variant="outline-light" onClick={() => setPokemonType("")}>
-              Reset Types
-            </Button>
+              <FaSearch className="search-icon" />
+            </div>
           </div>
         </Col>
       </Row>
--- src/components/TypeCarousel.tsx
+++ src/components/TypeCarousel.tsx
@@ -1,25 +1,14 @@
 import React from "react";
-import { Carousel, Image } from "react-bootstrap";
+import { Carousel, CarouselItem, Image, Figure } from "react-bootstrap";
+import { checkTypes } from "../utils/utils";
 
-const types = [
-  "bug",
-  "dragon",
-  "electric",
-  "fairy",
-  "fighting",
-  "fire",
-  "flying",
-  "ghost",
-  "grass",
-  "ground",
-  "ice",
-  "normal",
-  "poison",
-  "psychic",
-  "rock",
-  "steel",
-  "water",
-];
+const typeSet1 = ["bug", "dragon", "electric", "fairy", "fighting"];
+
+const typeSet2 = ["fire", "flying", "ghost", "grass"];
+
+const typeSet3 = ["ground", "ice", "normal", "poison"];
+
+const typeSet4 = ["psychic", "rock", "steel", "water"];
 
 interface TypeCarouselProps {
   setPokemonType: (type: string) => void;
@@ -29,117 +18,75 @@ function TypeCarousel({ setPokemonType }: TypeCarouselProps) {
   return (
     <Carousel indicators={false} interval={null}>
       <Carousel.Item>
-        <div className="d-flex justify-content-center gap-2">
-          <Image
-            src="images/bug-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("bug")}
-          />
-          <Image
-            src="images/dragon-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("dragon")}
-          />
-          <Image
-            src="images/electric-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("electric")}
-          />
-          <Image
-            src="images/fairy-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("fairy")}
-          />
-          <Image
-            src="images/fighting-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("fighting")}
-          />
-          <Image
-            src="images/fire-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("fire")}
-          />
+        <div className="d-flex justify-content-center gap-3">
+          {typeSet1.map((type) => (
+            <Figure>
+              <Figure.Image
+                key={type}
+                src={checkTypes(type)}
+                width={50}
+                className="type-icons"
+                onClick={() => setPokemonType(type)}
+              />
+              <Figure.Caption className="text-center text-capitalize">
+                {type}
+              </Figure.Caption>
+            </Figure>
+          ))}
         </div>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="d-flex justify-content-center gap-2">
-          <Image
-            src="images/flying-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("flying")}
-          />
-          <Image
-            src="images/ghost-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("ghost")}
-          />
-          <Image
-            src="images/grass-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("grass")}
-          />
-          <Image
-            src="images/ground-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("ground")}
-          />
-          <Image
-            src="images/ice-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("ice")}
-          />
-          <Image
-            src="images/normal-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("normal")}
-          />
+        <div className="d-flex justify-content-center gap-3">
+          {typeSet2.map((type) => (
+            <Figure>
+              <Figure.Image
+                key={type}
+                src={checkTypes(type)}
+                width={50}
+                className="type-icons"
+                onClick={() => setPokemonType(type)}
+              />
+              <Figure.Caption className="text-center text-capitalize">
+                {type}
+              </Figure.Caption>
+            </Figure>
+          ))}
         </div>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="d-flex justify-content-center gap-2">
-          <Image
-            src="images/poison-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("poison")}
-          />
-          <Image
-            src="images/psychic-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("psychic")}
-          />
-          <Image
-            src="images/rock-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("rock")}
-          />
-          <Image
-            src="images/steel-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("steel")}
-          />
-          <Image
-            src="images/water-type.png"
-            width={50}
-            className="type-icons"
-            onClick={() => setPokemonType("water")}
-          />
+        <div className="d-flex justify-content-center gap-3">
+          {typeSet3.map((type) => (
+            <Figure>
+              <Figure.Image
+                key={type}
+                src={checkTypes(type)}
+                width={50}
+                className="type-icons"
+                onClick={() => setPokemonType(type)}
+              />
+              <Figure.Caption className="text-center text-capitalize">
+                {type}
+              </Figure.Caption>
+            </Figure>
+          ))}
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="d-flex justify-content-center gap-3">
+          {typeSet4.map((type) => (
+            <Figure>
+              <Figure.Image
+                key={type}
+                src={checkTypes(type)}
+                width={50}
+                className="type-icons"
+                onClick={() => setPokemonType(type)}
+              />
+              <Figure.Caption className="text-center text-capitalize">
+                {type}
+              </Figure.Caption>
+            </Figure>
+          ))}
         </div>
       </Carousel.Item>
     </Carousel>
